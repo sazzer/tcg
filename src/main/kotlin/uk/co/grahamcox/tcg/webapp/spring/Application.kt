47e245e1747c35b5954b6668ab84cc3579ab1b26
Graham Cox <graham@grahamcox.co.uk>
2017-01-03 09:12:18 +0000

    Initial project setup, including Kotlin, Spring Boot and Jackson

---
+++ src/main/kotlin/uk/co/grahamcox/tcg/webapp/spring/Application.kt
@@ -0,0 +1,22 @@
+package uk.co.grahamcox.tcg.webapp.spring
+
+import de.codecentric.boot.admin.config.EnableAdminServer
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+/**
+ * The main entry point to the application
+ */
+@EnableAutoConfiguration
+@Configuration
+@EnableAdminServer
+internal open class Application
+
+/**
+ * Run the application
+ * @param args The command line arguments
+ */
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
