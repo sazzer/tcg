b331343fe395f0bec942163195070246e6f97969
Graham Cox <graham@grahamcox.co.uk>
2017-01-09 09:18:39 +0000

    Controller to act as a fake Google Authentication Service

---
+++ src/main/kotlin/uk/co/grahamcox/tcg/webapp/authentication/FakeGoogleController.kt
@@ -0,0 +1,65 @@
+package uk.co.grahamcox.tcg.webapp.authentication
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.View
+import org.springframework.web.servlet.view.RedirectView
+import org.springframework.web.util.UriComponentsBuilder
+import java.util.*
+
+/**
+ * Controller to act as a fake Google Authentication Service
+ */
+@RestController
+@RequestMapping("/api/authentication/fake/google")
+class FakeGoogleController {
+    /**
+     * Handle the Authorixation Code request
+     * @param redirectUri The Redirect URI to use
+     * @param state The nonce
+     * @return the redirect
+     */
+    @RequestMapping("/auth")
+    fun authorizationCode(@RequestParam("redirect_uri") redirectUri: String,
+                          @RequestParam(value = "state", required = false) state: String?) : View {
+        val redirectTo = UriComponentsBuilder.fromUriString(redirectUri)
+                .queryParam("state", state)
+                .queryParam("code", UUID.randomUUID().toString())
+                .queryParam("prompt", "consent")
+                .build()
+                .toUri()
+
+        return RedirectView(redirectTo.toString())
+    }
+
+    @RequestMapping(value = "/token", method = arrayOf(RequestMethod.POST))
+    fun token() = mapOf(
+            "access_token" to UUID.randomUUID().toString(),
+            "token_type" to "Bearer",
+            "expires_in" to 3600
+    )
+
+    @RequestMapping("/user")
+    fun userProfile() = mapOf(
+            "kind" to "plus#person",
+            "emails" to listOf(
+                    mapOf(
+                            "value" to "graham@grahamcox.co.uk",
+                            "type" to "account"
+                    )
+            ),
+            "id" to "1234567890",
+            "displayName" to "Graham Cox",
+            "name" to mapOf(
+                    "familyName" to "Cox",
+                    "givenName" to "Graham"
+            ),
+            "image" to mapOf(
+                    "url" to "https://lh4.googleusercontent.com/-puwiw-lmxTk/AAAAAAAAAAI/AAAAAAAAACU/6LRVNzR9b2o/photo.jpg?sz=50",
+                    "isDefault" to false
+            ),
+            "verified" to false
+    )
+}
--- src/main/kotlin/uk/co/grahamcox/tcg/webapp/spring/GoogleAuthenticationConfig.kt
+++ src/main/kotlin/uk/co/grahamcox/tcg/webapp/spring/GoogleAuthenticationConfig.kt
@@ -3,10 +3,12 @@ package uk.co.grahamcox.tcg.webapp.spring
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import uk.co.grahamcox.tcg.authentication.NonceGenerator
 import uk.co.grahamcox.tcg.authentication.ServletRedirectGenerator
 import uk.co.grahamcox.tcg.authentication.google.GoogleAuthenticationProvider
 import uk.co.grahamcox.tcg.authentication.google.GoogleConfig
+import uk.co.grahamcox.tcg.webapp.authentication.FakeGoogleController
 import java.net.URI
 
 /**
@@ -14,6 +16,10 @@ import java.net.URI
  */
 @Configuration
 class GoogleAuthenticationConfig {
+    @Bean
+    @Profile("test", "dev")
+    fun fakeGoogleService() = FakeGoogleController()
+
     @Bean
     fun googleConfig(@Value("\${authentication.google.clientId}") clientId: String,
                      @Value("\${authentication.google.clientSecret}") clientSecret: String,
