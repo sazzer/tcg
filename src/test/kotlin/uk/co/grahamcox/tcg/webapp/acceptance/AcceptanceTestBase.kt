ea436061e762575fd678a3681ed97ee0f0ee1349
Graham Cox <graham@grahamcox.co.uk>
2017-01-16 18:59:02 +0000

    Base class for all Acceptance Tests, to contain all of the Spring-ness

---
+++ src/test/kotlin/uk/co/grahamcox/tcg/webapp/acceptance/AcceptanceTestBase.kt
@@ -0,0 +1,53 @@
+package uk.co.grahamcox.tcg.webapp.acceptance
+
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.neo4j.driver.v1.AccessMode
+import org.neo4j.driver.v1.Driver
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.SpringBootDependencyInjectionTestExecutionListener
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener
+import org.springframework.test.context.web.ServletTestExecutionListener
+import uk.co.grahamcox.tcg.neo4j.executeStatement
+import uk.co.grahamcox.tcg.webapp.spring.Application
+
+/**
+ * Base class for all Acceptance Tests
+ */
+@ContextConfiguration(classes = arrayOf(AcceptanceTestConfiguration::class))
+@SpringBootTest(classes = arrayOf(Application::class), webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@RunWith(SpringRunner::class)
+@TestExecutionListeners(
+        DirtiesContextTestExecutionListener::class,
+        SpringBootDependencyInjectionTestExecutionListener::class,
+        DirtiesContextBeforeModesTestExecutionListener::class,
+        ServletTestExecutionListener::class
+)
+open class AcceptanceTestBase {
+
+    companion object {
+        /** The logger to use */
+        private val LOG = LoggerFactory.getLogger(AcceptanceTestBase::class.java)
+    }
+
+    /** The database driver. Note - don't do this */
+    @Autowired
+    private lateinit var driver: Driver
+
+    /**
+     * Reset the database before each test
+     */
+    @Before
+    fun cleanDatabase() {
+        driver.executeStatement("MATCH (n) OPTIONAL MATCH (n)-[r]-() WITH n,r DELETE n,r", mapOf(), AccessMode.WRITE)
+        LOG.debug("Deleted all nodes and relationships")
+    }
+}
--- src/test/kotlin/uk/co/grahamcox/tcg/webapp/acceptance/HealthcheckIT.kt
+++ src/test/kotlin/uk/co/grahamcox/tcg/webapp/acceptance/HealthcheckIT.kt
@@ -2,48 +2,18 @@ package uk.co.grahamcox.tcg.webapp.acceptance
 
 import com.jayway.jsonpath.JsonPath
 import com.winterbe.expekt.should
-import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.neo4j.driver.v1.AccessMode
 import org.neo4j.driver.v1.Driver
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.SpringBootDependencyInjectionTestExecutionListener
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.TestExecutionListeners
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener
-import org.springframework.test.context.web.ServletTestExecutionListener
 import uk.co.grahamcox.tcg.neo4j.executeStatement
-import uk.co.grahamcox.tcg.webapp.acceptance.AcceptanceTestConfiguration
-import uk.co.grahamcox.tcg.webapp.acceptance.Requester
-import uk.co.grahamcox.tcg.webapp.spring.Application
 import java.util.*
 
 /**
  * Acceptance Test for the Healthchecks
  */
-@ContextConfiguration(classes = arrayOf(AcceptanceTestConfiguration::class))
-@SpringBootTest(classes = arrayOf(Application::class), webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@RunWith(SpringRunner::class)
-@TestExecutionListeners(
-        DirtiesContextTestExecutionListener::class,
-        SpringBootDependencyInjectionTestExecutionListener::class,
-        DirtiesContextBeforeModesTestExecutionListener::class,
-        ServletTestExecutionListener::class
-)
-class HealthcheckIT {
-    companion object {
-        /** The logger to use */
-        private val LOG = LoggerFactory.getLogger(HealthcheckIT::class.java)
-    }
-
+class HealthcheckIT : AcceptanceTestBase() {
     /** The requester to use */
     @Autowired
     private lateinit var requester: Requester
@@ -51,12 +21,6 @@ class HealthcheckIT {
     /** The database driver. Note - don't do this */
     @Autowired
     private lateinit var driver: Driver
-
-    @Before
-    fun cleanDatabase() {
-        driver.executeStatement("MATCH (n) OPTIONAL MATCH (n)-[r]-() WITH n,r DELETE n,r", mapOf(), AccessMode.WRITE)
-        LOG.debug("Deleted all nodes and relationships")
-    }
 
     @Test
     fun `Checking the healthchecks with an empty database`() {
