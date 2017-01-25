da473499ee46fbb9b51ba722f612f5378f908c79
Graham Cox <graham@grahamcox.co.uk>
2017-01-25 16:56:57 +0000

    Internal representation of a stat

---
+++ src/main/kotlin/uk/co/grahamcox/tcg/stats/StatData.kt
@@ -0,0 +1,11 @@
+package uk.co.grahamcox.tcg.stats
+
+/**
+ * Representation of a single statistic
+ * @property name The name of the statistic
+ * @property description The description of the statistic
+ */
+class StatData(
+        val name: String,
+        val description: String
+)
