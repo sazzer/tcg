25e2a8c31fbbca7b6ad34365cd55958583858a5c
Graham Cox <graham@grahamcox.co.uk>
2017-02-18 09:45:44 +0000

    Set up ESLint

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -1,5 +1,7 @@
 import { logger } from './second';
 
+/** A name to use */
 const name = 'TCG';
 
+/* eslint-disable no-console */
 console.log(logger(name));
--- src/main/javascript/second.js
+++ src/main/javascript/second.js
@@ -1,3 +1,8 @@
+/**
+ * A function
+ * @param {string} n A parameter
+ * @returns {string} A return value
+ */
 export function logger(n) {
     return `Hello, ${n}`;
 }
