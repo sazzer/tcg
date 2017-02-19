db6b0c6a2ee77846b2f96052ac5559eacbdff672
Graham Cox <graham@grahamcox.co.uk>
2017-02-19 12:33:21 +0000

    Added in JSS for Local CSS

--- src/main/javascript/ui.jsx
+++ src/main/javascript/ui.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import injectSheet from 'react-jss';
+
+const styles = {
+    container: {
+        backgroundColor: 'yellow'
+    },
+    text: {
+        color: 'red'
+    }
+}
 
 /**
  * Main UI for the entire application
  */
+@injectSheet(styles)
 export default class UI extends React.Component {
     /**
      * Render the UI
      * @returns {React.Component} The React DOM to render
      */
     render() {
-        return <p>Hello React!</p>;
+        const { classes } = this.props;
+
+        return <p className={classes.container}>
+            <span className={classes.text}>Hello React!</span>
+        </p>;
     }
 }
