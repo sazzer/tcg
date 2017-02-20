85dc7934dc108148fc303386cbf732544701172c
Graham Cox <graham@grahamcox.co.uk>
2017-02-20 08:26:25 +0000

    Added in React Router

--- src/main/javascript/ui.jsx
+++ src/main/javascript/ui.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import injectSheet from 'react-jss';
 import { translate } from 'react-i18next';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const styles = {
     container: {
@@ -15,19 +16,70 @@ const styles = {
  * Main UI for the entire application
  */
 @injectSheet(styles)
+class Page1 extends React.Component {
+    render() {
+        const { classes } = this.props;
+
+        return <p className={ classes.container }>
+            <span className={ classes.text }>
+                <ul>
+                    <li><Link to="/page2">Page 2</Link></li>
+                    <li><Link to="/page3">Page 3</Link></li>
+                </ul>
+            </span>
+        </p>;
+    }
+}
+
+/**
+ * Main UI for the entire application
+ */
+@injectSheet(styles)
 @translate()
+class Page2 extends React.Component {
+    render() {
+        const { t, classes } = this.props;
+
+        return <p className={ classes.container }>
+            <span className={ classes.text }>
+                Page 2: { t('hello') }
+            </span>
+        </p>;
+    }
+}
+
+/**
+ * Main UI for the entire application
+ */
+@injectSheet(styles)
+@translate()
+class Page3 extends React.Component {
+    render() {
+        const { t, classes } = this.props;
+
+        return <p className={ classes.container }>
+            <span className={ classes.text }>
+                Page 3: { t('hello') }
+            </span>
+        </p>;
+    }
+}
+
+/**
+ * Main UI for the entire application
+ */
 export default class UI extends React.Component {
     /**
      * Render the UI
      * @returns {React.Component} The React DOM to render
      */
     render() {
-        const { t, classes } = this.props;
-
-        return <p className={ classes.container }>
-            <span className={ classes.text }>
-                { t('hello') }
-            </span>
-        </p>;
+        return <Router basename="/game" forceRefresh={false}>
+            <div>
+                <Route exact path="/" component={Page1} />
+                <Route exact path="/page2" component={Page2} />
+                <Route exact path="/page3" component={Page3} />
+            </div>
+        </Router>;
     }
 }
