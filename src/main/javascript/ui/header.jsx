2695ea6f51942f3e24166cb7a6d5e02f35112641
Graham Cox <graham@grahamcox.co.uk>
2017-02-22 09:51:04 +0000

    Reworked header bar in Bootstrap

--- src/main/javascript/ui/header.jsx
+++ src/main/javascript/ui/header.jsx
@@ -7,29 +7,10 @@ import Immutable from 'immutable';
  * Component representing the header of the application
  */
 @translate()
-@connect(state => ({
+@connect((state) => ({
     menu: state.get('menu', Immutable.List()).toJS(),
 }))
 export default class Header extends React.Component {
-    /**
-     * Construct the header
-     * @param {Object} props the props for the component
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false
-        };
-    }
-
-    /**
-     * Toggle whether the right menu is open or not
-     */
-    toggleRightMenu() {
-        this.setState((prevState) => ({
-            open: !prevState.open
-        }));
-    }
     /**
      * Actually render the header bar
      * @returns {React.Component} the header bar
@@ -37,32 +18,22 @@ export default class Header extends React.Component {
     render() {
         const { t, menu } = this.props;
 
-        const rightMenuClasses = [
-            "nav-right",
-            "nav-menu",
-            this.state.open ? "is-active" : ""
-        ].join(" ");
-
         const menuItems = menu.map((i) =>
-            <a className="nav-item">{i}</a>
+            <li className="nav-item"><a className="nav-link">{i}</a></li>
         );
 
-        return <nav className="nav">
-            <div className="nav-left">
-                <h1 className="nav-item title">{ t('page.title') }</h1>
-            </div>
+        return <nav className="navbar navbar-toggleable-md navbar-light bg-faded fixed-top">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand">{ t('page.title') }</a>
 
-            <div className="nav-center">
-            </div>
-
-            <div className="nav-toggle" onClick={ this.toggleRightMenu.bind(this) }>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-
-            <div className={ rightMenuClasses }>
-                { menuItems }
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                </ul>
+                <ul className="navbar-nav">
+                    { menuItems }
+                </ul>
             </div>
         </nav>;
     }
