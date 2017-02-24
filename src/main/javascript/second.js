ecd10642a9e1f715c7b88288ccac4ec9cbb8ee98
Graham Cox <graham@grahamcox.co.uk>
2017-02-24 14:13:18 +0000

    Decided to stop doing UI Unit tests

--- src/main/javascript/second.js
+++
@@ -1,8 +0,0 @@
-/**
- * A function
- * @param {string} n A parameter
- * @returns {string} A return value
- */
-export function logger(n) {
-    return `Hello, ${n}`;
-}
--- src/test/javascript/second.test.js
+++
@@ -1,6 +0,0 @@
-import { logger } from './second';
-import test from 'ava';
-
-test('second.logger', (t) => {
-    t.true('Hello, Graham' === logger('Graham'));
-});
